59e5690385e9ae5db5e64d50d7cdbbf74397e764
Damien <Damino95@gmail.com>
2022-09-27 15:37:45 +0200

    feat:add unit test with pest

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+it('try wrong credential to login', function () {
+    $response = $this->get('api/user/1');
+    $response->assertStatus(401);
+});
+
+it('unauthenticated user cannot get userDetail', function () {
+    $response = $this->get('api/user/1');
+    $response->assertStatus(401);
+});
+
+it('user authenticated  can get userDetail', function () {
+    // ActingAs permet de simuler le fait d'être un utilisateur
+    $response = $this->actingAs(\App\Models\User::factory()->create())
+    ->get('api/user/1');
+    $response->assertStatus(200);
+});
